85d97162390326ef1d5568636bc24bab84cbdb3d
Wenderson <wps713@gmail.com>
2020-03-17 08:55:15 -0300

    Inital project

---
+++ mobile/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+import './src/config/ReacotronConfig';
+const App = () => (
+  <View >
+    <Text>Hello World</Text>
+  </View >
+);
+
+
+export default App;
