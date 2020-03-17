1e03ea1cd442621b668e06afc8a55976c2662356
Wenderson <wps713@gmail.com>
2020-03-17 15:01:28 -0300

    Setting up status bar

--- mobile/src/Index.js
+++ mobile/src/Index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 import App from './App';
 
 export default function Index() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#7d40e7" />
       <App />
     </NavigationContainer>
   );
