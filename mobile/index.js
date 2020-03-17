95b6c1c892912a802d7cf069a056bd744dcbc93f
Wenderson <wps713@gmail.com>
2020-03-17 11:48:42 -0300

    Setting up react navigation

--- mobile/index.js
+++ mobile/index.js
@@ -1,5 +1,6 @@
 import './src/config/ReacotronConfig';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Index from './src/Index';
 import { name as appName } from './app.json';
-AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerComponent(appName, () => Index);
---
+++ mobile/src/routes.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+import SignIn from './pages/SignIn';
+import Delivery from './pages/Delivery';
+import Profile from './pages/Profile'
+
+const Stack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+export default function createRouter(isSigned = false) {
+  return !isSigned ? (
+    <>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={SignIn} />
+      </Stack.Navigator>
+    </>
+  ) : (
+      <Tabs.Navigator
+
+      >
+        <Tabs.Screen name="Delivery" component={Delivery} />
+        <Tabs.Screen name="Profile" component={Profile} />
+
+      </Tabs.Navigator>
+    )
+}
