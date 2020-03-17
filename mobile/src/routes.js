eae6ae75bea45c30450279a299b4d403f8eff442
Wenderson <wps713@gmail.com>
2020-03-17 16:30:57 -0300

    Customizing tab bar navigation

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import SignIn from './pages/SignIn';
 import Delivery from './pages/Delivery';
 import Profile from './pages/Profile';
@@ -18,9 +20,33 @@ export default function createRouter(isSigned = false) {
       </Stack.Navigator>
     </>
   ) : (
-    <Tabs.Navigator>
-      <Tabs.Screen name="Delivery" component={Delivery} />
-      <Tabs.Screen name="Profile" component={Profile} />
-    </Tabs.Navigator>
-  );
+      <Tabs.Navigator
+        tabBarOptions={{
+          activeTintColor: '#7D40E7',
+          inactiveTintColor: '#999999',
+          keyboardHidesTabBar: true,
+        }}
+      >
+        <Tabs.Screen
+          name="Delivery"
+          component={Delivery}
+          options={{
+            tabBarLabel: 'Entregas',
+            tabBarIcon: ({ color }) => (
+              <Icon name="reorder" size={25} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: 'Meu Perfil',
+            tabBarIcon: ({ color }) => (
+              <Icon name="account-circle" size={25} color={color} />
+            ),
+          }}
+        />
+      </Tabs.Navigator>
+    );
 }
