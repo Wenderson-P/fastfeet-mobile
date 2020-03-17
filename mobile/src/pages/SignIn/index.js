62fb81e6fc9bd2083b1c020cae58700c21e4da25
Wenderson <wps713@gmail.com>
2020-03-17 11:31:39 -0300

    Add initial pages

---
+++ mobile/src/pages/SignIn/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <View>
+      <Text>
+        SignIn
+     </Text>
+    </View>
+  );
+}
