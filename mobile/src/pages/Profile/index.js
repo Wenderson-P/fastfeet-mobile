beba6a87e7c8958290becf6a8034c7dc637752a6
Wenderson <wps713@gmail.com>
2020-03-17 15:38:35 -0300

    Add profile page content

--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import { Container, Form, FieldData, Logout, FieldLabel } from './styles';
 
 export default function Profile() {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <Container>
+      <Form>
+        <FieldLabel>Nome completo</FieldLabel>
+        <FieldData>Gaspar Antunes</FieldData>
+        <FieldLabel>Email</FieldLabel>
+        <FieldData>example@rocketseat.com.br</FieldData>
+        <FieldLabel>Data de cadatro</FieldLabel>
+        <FieldData>10/01/2020</FieldData>
+        <Logout>Logout</Logout>
+      </Form>
+    </Container>
   );
 }
---
+++ mobile/src/pages/Profile/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+
+import Button from '~/components/Button';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Form = styled.View`
+  align-self: stretch;
+  margin: 0 30px;
+`;
+
+export const FieldLabel = styled.Text`
+  font-size: 12px;
+  text-align: left;
+  margin-top: 15px;
+  font-weight: 300;
+`;
+
+export const FieldData = styled.Text`
+  font-size: 22px;
+  text-align: left;
+  font-weight: bold;
+`;
+
+export const Logout = styled(Button)`
+  margin-top: 30px;
+  background-color: #e74040;
+  text-align: center;
+`;
