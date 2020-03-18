449519a1564660f440e46fe5ed080bfab9a7b3ab
Wenderson <wps713@gmail.com>
2020-03-18 12:40:54 -0300

    Add content to delivery page

--- mobile/src/pages/Delivery/index.js
+++ mobile/src/pages/Delivery/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Welcome,
+  UserName,
+  LogoutButton,
+  DeliveryHeader,
+  Text,
+  Filters,
+  FilterOption,
+} from './styles';
+
+import avatar from '~/assets/tempAvatar.png';
+
+import DeliveryInfo from '~/components/DeliveryInfo';
 
 export default function Delivery() {
   return (
-    <View>
-      <Text>Delivery</Text>
-    </View>
+    <Container>
+      <Header>
+        <Avatar source={avatar} />
+        <Welcome>Bem vindo de volta,</Welcome>
+        <UserName>Wenderson Pacheco</UserName>
+        <LogoutButton />
+      </Header>
+      <DeliveryHeader>
+        <Text>Entregas</Text>
+        <Filters>
+          <FilterOption>Pendentes</FilterOption>
+          <FilterOption>Entregues</FilterOption>
+        </Filters>
+      </DeliveryHeader>
+      <DeliveryInfo />
+      <DeliveryInfo />
+    </Container>
   );
 }
---
+++ mobile/src/pages/Delivery/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background-color: #ffffff;
+  flex: 1;
+`;
+
+export const Avatar = styled.Image``;
+
+export const Header = styled.View``;
+
+export const Welcome = styled.Text``;
+
+export const UserName = styled.Text``;
+
+export const LogoutButton = styled.Text``;
+
+export const DeliveryHeader = styled.View``;
+
+export const Text = styled.Text``;
+
+export const Filters = styled.View``;
+
+export const FilterOption = styled.Text``;
