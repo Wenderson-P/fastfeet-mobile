638742078f81cd24cafcd7457efef77bf48eb25d
Wenderson <wps713@gmail.com>
2020-03-20 12:01:16 -0300

    Passing navigation from delivery page to delivery info

--- mobile/src/pages/Delivery/index.js
+++ mobile/src/pages/Delivery/index.js
@@ -17,7 +17,7 @@ import {
   FilterOption,
 } from './styles';
 
-export default function Delivery() {
+export default function Delivery({ navigation }) {
   return (
     <Background>
       <Container>
@@ -33,8 +33,7 @@ export default function Delivery() {
             <FilterOption>Entregues</FilterOption>
           </Filters>
         </DeliveryHeader>
-        <DeliveryInfo />
-        <DeliveryInfo />
+        <DeliveryInfo navigation={navigation} />
       </Container>
     </Background>
   );
