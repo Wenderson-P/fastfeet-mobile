8cb79267f88d406f4d7476eb12434356a5ecdd7d
Wenderson <wps713@gmail.com>
2020-03-20 16:06:27 -0300

    Add buttons in delivery details

--- mobile/src/pages/DeliveryDetails/index.js
+++ mobile/src/pages/DeliveryDetails/index.js
@@ -2,14 +2,37 @@ import React from 'react';
 
 import DeliveryInfoCard from '~/components/DeliveryInfoCard';
 import DeliverySituationCard from '~/components/DeliverySituationCard';
+import DeliveryActionButton from '~/components/DeliveryActionButton';
 
-import { Container } from './styles';
+import { Container, Buttons, Line } from './styles';
 
 export default function DeliveryDetails() {
   return (
     <Container>
       <DeliveryInfoCard />
       <DeliverySituationCard />
+      <Buttons>
+        <DeliveryActionButton
+          iconName="highlight-off"
+          iconColor="#E74040"
+          iconSize={24}
+          title="Informar Problema"
+        />
+        <Line />
+        <DeliveryActionButton
+          iconName="info-outline"
+          iconColor="#E7BA40"
+          iconSize={24}
+          title="Visualizar Problemas"
+        />
+        <Line />
+        <DeliveryActionButton
+          iconName="check-circle"
+          iconColor="#7D40E7"
+          iconSize={24}
+          title="Confirmar Entrega"
+        />
+      </Buttons>
     </Container>
   );
 }
--- mobile/src/pages/DeliveryDetails/styles.js
+++ mobile/src/pages/DeliveryDetails/styles.js
@@ -1,3 +1,17 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  margin: 20px 20px 0px 20px;
+`;
+
+export const Buttons = styled.View`
+  margin-top: 10px;
+  flex-direction: row;
+  width: 33%;
+`;
+
+export const Line = styled.View`
+  height: 100%;
+  width: 1px;
+  background-color: #e5e5e5;
+`;
