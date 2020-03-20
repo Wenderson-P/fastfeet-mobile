4c66487c9368c3c403f031b1e543158e980b2be5
Wenderson <wps713@gmail.com>
2020-03-20 13:10:15 -0300

    Add info card style

--- mobile/src/components/DeliveryInfoCard/styles.js
+++ mobile/src/components/DeliveryInfoCard/styles.js
@@ -1,13 +1,32 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  padding: 15px 5px 0px 14px;
+  background-color: #ffffff;
+  border-radius: 4px;
+`;
 
-export const CardHeader = styled.View``;
+export const CardHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
 
-export const HeaderLabel = styled.Text``;
+export const HeaderLabel = styled.Text`
+  color: #7d40e7;
+  font-weight: bold;
+  margin-left: 5px;
+`;
 
 export const Row = styled.View``;
 
-export const RowLabel = styled.Text``;
+export const RowLabel = styled.Text`
+  color: #999999;
+  font-weight: bold;
 
-export const RowData = styled.Text``;
+  text-transform: uppercase;
+`;
+
+export const RowData = styled.Text`
+  color: #666666;
+  margin-bottom: 15px;
+`;
