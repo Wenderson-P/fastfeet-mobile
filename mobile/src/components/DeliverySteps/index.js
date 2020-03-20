83e76569a260ec3bfaae3a916a936a2329bf2be6
Wenderson <wps713@gmail.com>
2020-03-20 12:00:11 -0300

    Creating custom step component
    The progress-step lib was removed

--- mobile/src/components/DeliverySteps/index.js
+++ mobile/src/components/DeliverySteps/index.js
@@ -1,23 +1,40 @@
 import React from 'react';
-import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
-
-import { Container } from './styles';
+import { Container, Step, StepRow, Line, StepLabel, Row } from './styles';
 
 export default function DeliverySteps({ activeStep }) {
   return (
     <Container>
-      <ProgressSteps
-        activeStep={activeStep}
-        progressBarColor="#7D40E7"
-        completedProgressBarColor="#7D40E7"
-        completedStepIconColor="#7D40E7"
-        borderWidth={1}
-        activeStepIconBorderColor="#7D40E7"
-      >
-        <ProgressStep label="Aguardando Retirada" />
-        <ProgressStep label="Retirada" />
-        <ProgressStep label="Entregue" />
-      </ProgressSteps>
+      <StepRow>
+        <Step activeStep={activeStep >= 1} />
+        <Line />
+        <Step activeStep={activeStep >= 2} />
+        <Line />
+        <Step activeStep={activeStep >= 3} />
+      </StepRow>
+      <Row>
+        <StepLabel>Aguardando{'\n'}Retirada</StepLabel>
+        <StepLabel>Retirada</StepLabel>
+        <StepLabel>Entregue</StepLabel>
+      </Row>
     </Container>
   );
 }
+
+// export default function DeliverySteps({ activeStep }) {
+//   return (
+//     <Container>
+//       <ProgressSteps
+//         activeStep={activeStep}
+//         progressBarColor="#7D40E7"
+//         completedProgressBarColor="#7D40E7"
+//         completedStepIconColor="#7D40E7"
+//         borderWidth={1}
+//         activeStepIconBorderColor="#7D40E7"
+//       >
+//         <ProgressStep label="Aguardando Retirada" />
+//         <ProgressStep label="Retirada" />
+//         <ProgressStep label="Entregue" />
+//       </ProgressSteps>
+//     </Container>
+//   );
+// }
--- mobile/src/components/DeliverySteps/styles.js
+++ mobile/src/components/DeliverySteps/styles.js
@@ -1,3 +1,38 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  width: 50px;
+  width: 100%;
+  align-self: center;
+`;
+
+export const StepRow = styled.View`
+  padding: 0px 10px 2px 15px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+export const Step = styled.View`
+  background: ${props => (props.activeStep ? '#7d40e7' : '#FFFFFF')};
+  border: 1px #7d40e7;
+  border-radius: 25px;
+  height: 9px;
+  width: 9px;
+`;
+
+export const Line = styled.View`
+  background: #7d40e7;
+  height: 2px;
+  width: 110px;
+`;
+
+export const StepLabel = styled.Text`
+  font-size: 8px;
+  color: #999999;
+`;
