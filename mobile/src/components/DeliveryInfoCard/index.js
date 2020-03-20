76495ab8f5660bd7fb097bf2ef268e76ea4bb854
Wenderson <wps713@gmail.com>
2020-03-20 12:35:01 -0300

    Add info card initial content

--- mobile/src/components/DeliveryInfoCard/index.js
+++ mobile/src/components/DeliveryInfoCard/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
 
-import { Container } from './styles';
+import {
+  Container,
+  CardHeader,
+  HeaderLabel,
+  Recipient,
+  Adress,
+  Product,
+} from './styles';
 
 export default function DeliveryInfoCard() {
-  return <View />;
+  return (
+    <Container>
+      <CardHeader>
+        <HeaderLabel>Informações da entrega</HeaderLabel>
+      </CardHeader>
+      <Recipient>Ludwig van Beethoven</Recipient>
+      <Adress>Rua Beethoven, 1729, Diadema - SP, 09960-580</Adress>
+      <Product>Yamaha SX7</Product>
+    </Container>
+  );
 }
--- mobile/src/components/DeliveryInfoCard/styles.js
+++ mobile/src/components/DeliveryInfoCard/styles.js
@@ -1,3 +1,12 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View``;
+export const CardHeader = styled.View``;
+
+export const HeaderLabel = styled.Text``;
+
+export const Recipient = styled.Text``;
+
+export const Adress = styled.Text``;
+
+export const Product = styled.Text``;
