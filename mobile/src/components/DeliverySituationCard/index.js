a3c7bd566311a80bf189cd425996545ee822ab8d
Wenderson <wps713@gmail.com>
2020-03-20 15:29:39 -0300

    Add content and style to situation card

--- mobile/src/components/DeliverySituationCard/index.js
+++ mobile/src/components/DeliverySituationCard/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import {
+  Container,
+  CardHeader,
+  HeaderLabel,
+  Row,
+  RowLabel,
+  RowData,
+  DateRow,
+} from './styles';
 
-export default function DeliverySituationCard() {
-  return <View />;
+export default function DeliveryInfoCard() {
+  return (
+    <Container>
+      <CardHeader>
+        <Icon name="event" size={24} color="#7D40E7" />
+        <HeaderLabel>Situação da entrega</HeaderLabel>
+      </CardHeader>
+      <Row>
+        <RowLabel>Status</RowLabel>
+        <RowData>Pendente</RowData>
+      </Row>
+      <DateRow>
+        <Row>
+          <RowLabel>Data de retirada</RowLabel>
+          <RowData>Yamaha SX7</RowData>
+        </Row>
+        <Row>
+          <RowLabel>Data de entrega</RowLabel>
+          <RowData>--/--/--</RowData>
+        </Row>
+      </DateRow>
+    </Container>
+  );
 }
--- mobile/src/components/DeliverySituationCard/styles.js
+++ mobile/src/components/DeliverySituationCard/styles.js
@@ -1,3 +1,37 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  padding: 15px 15px 0px 14px;
+  background-color: #ffffff;
+  border-radius: 4px;
+`;
+
+export const CardHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const HeaderLabel = styled.Text`
+  color: #7d40e7;
+  font-weight: bold;
+  margin-left: 5px;
+`;
+
+export const Row = styled.View``;
+
+export const RowLabel = styled.Text`
+  color: #999999;
+  font-weight: bold;
+
+  text-transform: uppercase;
+`;
+
+export const RowData = styled.Text`
+  color: #666666;
+  margin-bottom: 15px;
+`;
+
+export const DateRow = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
