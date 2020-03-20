d2115411acdbf80fc1c0423f072a9f0ff69705cf
Wenderson <wps713@gmail.com>
2020-03-20 16:06:15 -0300

    Add delivery action button component

---
+++ mobile/src/components/DeliveryActionButton/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, Text } from './styles';
+
+export default function DeliveryActionButton({
+  iconName,
+  iconColor,
+  iconSize,
+  title,
+  ...rest
+}) {
+  return (
+    <Container {...rest}>
+      <Icon name={iconName} size={iconSize} color={iconColor} />
+      <Text>{title}</Text>
+    </Container>
+  );
+}
+
+DeliveryActionButton.propTypes = {
+  children: PropTypes.string.isRequired,
+};
---
+++ mobile/src/components/DeliveryActionButton/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+import { BaseButton } from 'react-native-gesture-handler';
+
+export const Container = styled(BaseButton)`
+  border-radius: 5px;
+  background-color: #f8f9fd;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #f8f9fd;
+  box-shadow: 4px 8px 0 red;
+`;
+
+export const Text = styled.Text`
+  font-size: 12px;
+  color: #999999;
+  text-align: center;
+  margin: 0px 25px;
+`;
