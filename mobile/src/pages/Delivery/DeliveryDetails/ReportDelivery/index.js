64b1dfe9c5b636b8e28dd83ac66d284b57f1f0d3
Wenderson <wps713@gmail.com>
2020-04-13 17:50:13 -0300

    Add report a delivery problem

--- mobile/src/pages/Delivery/DeliveryDetails/ReportDelivery/index.js
+++ mobile/src/pages/Delivery/DeliveryDetails/ReportDelivery/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
 
 import { Container, TextField, SubmitButton } from './styles';
 
-export default function ReportDelivery() {
+import api from '~/services/api';
+
+export default function ReportDelivery({ route }) {
+  const { deliveryId } = route.params;
+  const [report, setReport] = useState('');
+
+  async function handleSubmit() {
+    if (report !== '') {
+      await api.post(`/delivery/${deliveryId}/problems`, {
+        description: report,
+      });
+      setReport('');
+      Alert.alert('Problema enviado');
+    }
+  }
+
   return (
     <Container>
-      <TextField placeholder="Inclua aqui o problema que ocorreu na entrega." />
-      <SubmitButton>Enviar</SubmitButton>
+      <TextField
+        placeholder="Inclua aqui o problema que ocorreu na entrega."
+        onChangeText={setReport}
+        value={report}
+      />
+      <SubmitButton onPress={() => handleSubmit()}>Enviar</SubmitButton>
     </Container>
   );
 }
