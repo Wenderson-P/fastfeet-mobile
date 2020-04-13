3cf32a7ed563d830b074d0d8bfd4830277fc7548
Wenderson <wps713@gmail.com>
2020-04-13 16:17:53 -0300

    Setting up redux, redux-persist in app

--- mobile/src/Index.js
+++ mobile/src/Index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import App from './App';
+import './config/ReacotronConfig';
+
+import { store, persistor } from './store';
 
 export default function Index() {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="light-content" backgroundColor="#7d40e7" />
-      <App />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#7d40e7" />
+          <App />
+        </PersistGate>
+      </Provider>
     </NavigationContainer>
   );
 }
