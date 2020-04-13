9039b5f64190dbc6988f70dabf52a7d9b264f9ee
Wenderson <wps713@gmail.com>
2020-04-13 16:18:35 -0300

    Add signIn Request action in SignIn page

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image } from 'react-native';
 
+import { useDispatch } from 'react-redux';
 import { Container, Form, FormInput, SubmitButton } from './styles';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.png';
 
 export default function SignIn() {
+  const dispatch = useDispatch();
+
+  const [id, setId] = useState('');
+
   return (
     <Container>
       <Image source={logo} style={{ width: 244, height: 48 }} />
@@ -14,8 +21,12 @@ export default function SignIn() {
           keyboardType="number-pad"
           placeholder="Informe seu ID de cadastro"
           returnKeyType="send"
+          onChangeText={setId}
+          value={id}
         />
-        <SubmitButton onPress={() => { }}>Entrar no sistema</SubmitButton>
+        <SubmitButton onPress={() => dispatch(signInRequest(id))}>
+          Entrar no sistema
+        </SubmitButton>
       </Form>
     </Container>
   );
