b0f58ded851aa1722a181b73e6521390a643250f
Wenderson <wps713@gmail.com>
2020-04-15 19:30:04 -0300

    Add a button to pickupDelivery in DeliveryDetails

--- mobile/src/pages/Delivery/DeliveryDetails/index.js
+++ mobile/src/pages/Delivery/DeliveryDetails/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useSelector } from 'react-redux';
 import DeliveryInfoCard from '~/components/DeliveryInfoCard';
 import DeliverySituationCard from '~/components/DeliverySituationCard';
 import DeliveryActionButton from '~/components/DeliveryActionButton';
 
-import { Container, Buttons, Line } from './styles';
+import api from '~/services/api';
+
+import {
+  Container,
+  Buttons,
+  Line,
+  PickUpDeliveryButton,
+  PickUpDeliveryText,
+} from './styles';
 
 export default function DeliveryDetails({ navigation, route }) {
   const { delivery } = route.params;
+  const auth = useSelector(state => state.auth);
 
   function handleConfirmDelivery() {
     if (delivery.start_date === null) {
@@ -17,39 +28,58 @@ export default function DeliveryDetails({ navigation, route }) {
     }
   }
 
+  async function handlePickUpDelivery() {
+    try {
+      await api.put(`/deliveryman/${auth.id}/deliveries?q=${delivery.id}`);
+      Alert.alert('Encomenda retirada com sucesso');
+      navigation.navigate('Delivery');
+    } catch (error) {
+      Alert.alert('Erro ao retirar entrega');
+    }
+  }
+
   return (
     <Container>
       <DeliveryInfoCard delivery={delivery} />
       <DeliverySituationCard delivery={delivery} />
-      <Buttons>
-        <DeliveryActionButton
-          iconName="highlight-off"
-          iconColor="#E74040"
-          iconSize={24}
-          title="Informar Problema"
-          onPress={() =>
-            navigation.navigate('ReportDelivery', { deliveryId: delivery.id })
-          }
-        />
-        <Line />
-        <DeliveryActionButton
-          iconName="info-outline"
-          iconColor="#E7BA40"
-          iconSize={24}
-          title="Visualizar Problemas"
-          onPress={() =>
-            navigation.navigate('DeliveryProblems', { deliveryId: delivery.id })
-          }
-        />
-        <Line />
-        <DeliveryActionButton
-          iconName="check-circle"
-          iconColor="#7D40E7"
-          iconSize={24}
-          title="Confirmar Entrega"
-          onPress={() => handleConfirmDelivery()}
-        />
-      </Buttons>
+      {delivery.start_date ? (
+        <Buttons>
+          <DeliveryActionButton
+            iconName="highlight-off"
+            iconColor="#E74040"
+            iconSize={24}
+            title="Informar Problema"
+            onPress={() =>
+              navigation.navigate('ReportDelivery', { deliveryId: delivery.id })
+            }
+          />
+          <Line />
+          <DeliveryActionButton
+            iconName="info-outline"
+            iconColor="#E7BA40"
+            iconSize={24}
+            title="Visualizar Problemas"
+            onPress={() =>
+              navigation.navigate('DeliveryProblems', {
+                deliveryId: delivery.id,
+              })
+            }
+          />
+          <Line />
+          <DeliveryActionButton
+            iconName="check-circle"
+            iconColor="#7D40E7"
+            iconSize={24}
+            title="Confirmar Entrega"
+            onPress={() => handleConfirmDelivery()}
+          />
+        </Buttons>
+      ) : (
+          <PickUpDeliveryButton onPress={() => handlePickUpDelivery()}>
+            <Icon name="local-shipping" size={30} color="#7D40E7" />
+            <PickUpDeliveryText> Retirar entrega</PickUpDeliveryText>
+          </PickUpDeliveryButton>
+        )}
     </Container>
   );
 }
--- mobile/src/pages/Delivery/DeliveryDetails/styles.js
+++ mobile/src/pages/Delivery/DeliveryDetails/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { BaseButton } from 'react-native-gesture-handler';
 
 export const Container = styled.ScrollView.attrs({
   showsVerticalScrollIndicator: false,
@@ -16,4 +17,21 @@ export const Line = styled.View`
   height: 100%;
   width: 1px;
   background-color: #e5e5e5;
+`;
+
+export const PickUpDeliveryButton = styled.TouchableOpacity`
+  width: 100%;
+  margin-top: 30px;
+  padding: 20px;
+  background-color: #f8f9fd;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  border: 1px solid #f8f9fd;
+`;
+
+export const PickUpDeliveryText = styled.Text`
+  text-align: center;
+  font-size: 16px;
+  color: #999999;
 `;
